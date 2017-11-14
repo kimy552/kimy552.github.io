8cd05de3f46c48a57d4f22d7c9622e972b145abf
kimy552 <31393384+kimy552@users.noreply.github.com>
2017-11-14 14:42:19 -0500

    sssss

--- creative-computing/events/events.js
+++ creative-computing/events/events.js
@@ -19,10 +19,6 @@ var blueButton = document.getElementById("car2")
 
 blueButton.addEventListener("click", handleBtnClick)
 
-function handleBtnClick() {
-    document.getElementById("car2").style.borderColor = "#4ddbff"
-    document.getElementById("car3").style.backgroundColor = "#4ddbff"
-    document.getElementById("wheel1").style.borderColor = "#4ddbff"
-    document.getElementById("wheel2").style.borderColor = "#4ddbff"
+function handleBtnClick(click,)
    
 }
